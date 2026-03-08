9d16ffe80424fc894b6185802ac30229a0bf56c8
Ujjwal2026creator <chmcs2026.ujjwal.pandey@gmail.com>
2026-03-08 17:55:36 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/internships'
+const API_URL = import.meta.env.VITE_API_URL || 'https://backend-3n91.onrender.com'
 
 const apiInstance = axios.create({
   baseURL: API_URL,
